d4f06d6289cda20a91d9164a45b6186177c5bc9b
Mihail Vasilchenko <mihail@geekguts.com>
2020-02-05 01:29:41 +0200

    add feed parse and word count functions

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,4 +25,42 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
+    /**
+     * Parse feed.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function parseFeed() {
+        $feed = \Feeds::make('https://www.theregister.co.uk/software/headlines.atom', true); // if RSS Feed has invalid mime types, force to read
+        $data = array(
+            'title'     => $feed->get_title(),
+            'permalink' => $feed->get_permalink(),
+            'items'     => $feed->get_items(),
+            'top'       => $this->countWords($feed->get_items()),
+        );
+
+        return view('home', $data);
+    }
+
+    public function convert_multi_array($array)
+    {
+        $out = '';
+        foreach ($array as $key => $value) {
+            $out .= strip_tags(strtolower($value->get_title() . ' ' . $value->get_description()));
+        }
+
+        return $out;
+    }
+
+    public function countWords($items) {
+        $string = $this->convert_multi_array($items);
+        mb_internal_encoding('UTF-8');
+        $stopwords = array('the', 'be', 'to', 'of', 'and', 'a', 'in', 'that', 'have', 'I', 'it', 'for', 'not', 'on', 'with', 'he', 'as', 'you', 'do', 'at', 'this', 'but', 'his', 'by', 'from', 'they', 'we', 'say', 'her', 'she', 'or', 'an', 'will', 'my', 'one', 'all', 'would', 'there', 'their', 'what', 'so', 'up', 'out', 'if', 'about', 'who', 'get', 'which', 'go', 'me', 'when', 'make', 'can', 'like', 'time', 'no', 'just', 'him', 'know', 'take', 'people', 'into', 'year', 'your', 'good', 'some', 'could', 'them', 'see', 'other', 'than', 'then', 'now', 'look', 'only', 'come', 'its', 'over', 'think', 'also', 'back', 'after', 'use', 'two', 'how', 'our', 'work', 'first', 'well', 'way', 'even', 'new', 'want', 'because', 'any', 'these', 'give', 'day', 'most', 'us', 'are', 'has', 'was'); // 'are', 'has', 'was'
+        $string = preg_replace('/[\pP]/u', '', trim(preg_replace('/\s\s+/iu', '', mb_strtolower($string))));
+        $matchWords = array_filter(explode(' ',$string) , function ($item) use ($stopwords) { return !($item == '' || in_array($item, $stopwords) || mb_strlen($item) <= 2 || is_numeric($item));});
+        $wordCountArr = array_count_values($matchWords);
+        arsort($wordCountArr);
+        return array_slice($wordCountArr, 0, 10);
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,31 @@
                     @endif
 
                     You are logged in!
+
+                    <div class="header">
+                        <h1>Top 10</h1>
+                    </div>
+
+                    @foreach ($top as $key => $word)
+                        <div class="item">
+                            <h2>{{ $word }}</h2>
+                            <p>{{ $key }}</p>
+                        </div>
+                    @endforeach
+
+                    <div class="header">
+                        <h1><a href="{{ $permalink }}">{{ $title }}</a></h1>
+                    </div>
+
+                    @foreach ($items as $item)
+                        <div class="item">
+                            <h2><a href="{{ $item->get_permalink() }}">{{ $item->get_title() }}</a></h2>
+                            {!! $item->get_description() !!}
+                            <p><small>Posted on {{ $item->get_date('j F Y | g:i a') }}</small></p>
+                        </div>
+                    @endforeach
+
+
                 </div>
             </div>
         </div>
