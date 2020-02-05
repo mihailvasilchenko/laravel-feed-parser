3ce51f7d329be13db8adee5821f7a4934da13099
Mihail Vasilchenko <mihail@geekguts.com>
2020-02-06 01:17:28 +0200

    added stying to templates and comments to methods

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,46 +2,39 @@
 
 @section('content')
 <div class="container">
+    @if (session('status'))
+        <div class="alert alert-success" role="alert">
+            {{ session('status') }}
+        </div>
+    @endif
+
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-4">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">{{ __('Top 10 Words') }}</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-
-                    <div class="header">
-                        <h1>Top 10</h1>
-                    </div>
-
-                    @foreach ($top as $key => $word)
-                        <div class="item">
-                            <h2>{{ $word }}</h2>
-                            <p>{{ $key }}</p>
-                        </div>
-                    @endforeach
-
-                    <div class="header">
-                        <h1><a href="{{ $permalink }}">{{ $title }}</a></h1>
-                    </div>
-
-                    @foreach ($items as $item)
-                        <div class="item">
-                            <h2><a href="{{ $item->get_permalink() }}">{{ $item->get_title() }}</a></h2>
-                            {!! $item->get_description() !!}
-                            <p><small>Posted on {{ $item->get_date('j F Y | g:i a') }}</small></p>
-                        </div>
-                    @endforeach
-
-
+                    <ol class="lead">
+                        @foreach ($top as $word => $count)
+                            <li>
+                                {{ $word }} <span class="badge badge-primary">{{ $count }}</span>
+                            </li>
+                        @endforeach
+                    </ol>
                 </div>
             </div>
+        </div>
+        <div class="col-md-8">
+            <h2 class="display-6 mb-4"><a href="{{ $permalink }}">{{ $title }}</a></h2>
+            @foreach ($items as $item)
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <h3><a href="{{ $item->get_link() }}">{{ $item->get_title() }}</a></h3>
+                        {!! $item->get_description() !!}
+                        <p class="mb-0"><small>Posted on {{ $item->get_date('j F Y | g:i a') }}</small></p>
+                    </div>
+                </div>
+            @endforeach
         </div>
     </div>
 </div>
